57db7ccb1a2455db27d04914ba8f3d5791a3ba84
moonwonki <moonwonki1999@gmail.com>
2024-03-10 13:42:11 +0900

    스프링 프레임워크 초기 세팅

---
+++ src/test/java/com/example/WebOrder/WebOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
