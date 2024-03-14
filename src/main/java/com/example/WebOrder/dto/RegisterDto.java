ffd39ff13e985223749d543b2e68f7d0ad2c05c2
moonwonki <moonwonki1999@gmail.com>
2024-03-14 17:45:52 +0900

    :sparkles: feat: 회원가입 위해 필요한 DTO 추가
    - RegisterDto 추가

---
+++ src/main/java/com/example/WebOrder/dto/RegisterDto.java
@@ -0,0 +1,11 @@
+package com.example.WebOrder.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDto {
+    private String username;
+    private String password;
+}
