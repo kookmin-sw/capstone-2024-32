1ff429612af127afc0c158957ac77bfdbb8acd61
hightuv <hightuv@gmail.com>
2024-03-16 16:58:35 +0900

    :sparkles: feat: Category 엔티티 구현
    - 하나의 카테고리에 여러 Item이 들어가는 구조로 설정

---
+++ src/main/java/com/example/WebOrder/entity/Category.java
@@ -0,0 +1,24 @@
+package com.example.WebOrder.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Item> items = new ArrayList<>();
+
+}
