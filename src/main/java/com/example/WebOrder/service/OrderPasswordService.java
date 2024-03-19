5b71dadd68804b57750ecc246c6ce7d9c3289fdb
moonwonki <moonwonki1999@gmail.com>
2024-03-20 00:13:08 +0900

    :sparkles: feat: OrderPasswordService 구현
    - 아직 미구현 상태
    - User 엔티티에 password 필드가 추가되면 그때 수정

---
+++ src/main/java/com/example/WebOrder/service/OrderPasswordService.java
@@ -0,0 +1,15 @@
+package com.example.WebOrder.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class OrderPasswordService {
+
+    Boolean isPasswordValid(Long userId, String password){
+
+        return true;
+    }
+}
+
