d442c02fbb302559c0d2956cd1843d14bae965c2
moonwonki <moonwonki1999@gmail.com>
2024-03-26 16:30:07 +0900

    :sparkles: feat. Login Controller 수정

--- src/main/java/com/example/WebOrder/controller/LoginController.java
+++ src/main/java/com/example/WebOrder/controller/LoginController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,34 +19,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class LoginController {
     private final LoginService loginService;
-    private final PasswordEncoder passwordEncoder;
 
-    public LoginController(LoginService loginService, PasswordEncoder passwordEncoder) {
+    public LoginController(LoginService loginService) {
         this.loginService = loginService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/index")
     public String getIndex(){
-        return "/html/index";
+        return "html/index";
     }
     @GetMapping("/login")
-    public String getLoginForm(@ModelAttribute("loginFormDto") LoginFormDto dto){
+    public String getLoginForm(){
         log.info("로그인 폼 소환");
         return "html/loginForm";
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute ("loginFormDto") LoginFormDto dto, BindingResult bindingResult){
+    public String login(@Valid LoginFormDto dto){
         log.info("로그인 시도");
         if (loginService.isLoginAttemptValid(dto)){
             log.info("로그인 성공");
-            return "html/index";
+            return "redirect:/index";
         }
         else {
             log.info("로그인 실패");
-            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
-            return "html/loginForm";
+            return "redirect:/login?error=true";
         }
     }
 
@@ -57,29 +55,22 @@ public class LoginController {
 
     @PostMapping("/register")
     public String register(@Valid UserFormDto dto, BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()){
+            log.info("validation 에러");
+            return "redirect:/register";
+        }
+
         log.info("회원가입 시도");
-        if (bindingResult.hasErrors()) {
-            log.info("회원가입 실패");
-            return "html/registerForm";
-        }
-
-        if (loginService.findByUsername(dto.getUsername()).isPresent()){
+        if (loginService.usernameExists(dto.getUsername())){
             log.info("username 중복");
-            return "redirect:/register?error=true";
-        }
-
-        if (!dto.getPassword().equals(dto.getPasswordCheck())) {
-            bindingResult.rejectValue("passwordCheck", "passwordNotSame", "비밀번호를 다시 확인해주세요.");
-            return "html/registerForm";
+            bindingResult.addError(new FieldError("UserFormDto", "username", "유저네임이 중복되었습니다."));
+            return "redirect:/register";
         }
         else {
+            loginService.createUser(dto);
             log.info("회원가입 성공");
-            User user = new User();
-            user.setUsername(dto.getUsername());
-            user.setPassword(passwordEncoder.encode(dto.getPassword()));
-            user.setName(dto.getName());
-            loginService.join(user);
-            return "redirect:/";
+            return "redirect:/login";
         }
     }
 }
