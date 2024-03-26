e3348f7cc5d35ac8ac1485a376d2c6c4582bf3bb
moonwonki <moonwonki1999@gmail.com>
2024-03-26 16:32:45 +0900

    :sparkles: feat. UserFormDto 변경

--- src/main/java/com/example/WebOrder/dto/UserFormDto.java
+++ src/main/java/com/example/WebOrder/dto/UserFormDto.java
@@ -15,9 +15,6 @@ public class UserFormDto {
     @Length(min = 8, max = 16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요.")
     private String password;
 
-    @NotEmpty(message = "비밀번호를 한 번 더 입력해주세요.")
-    private String passwordCheck;
-
     @NotEmpty(message = "이름은 필수 입력 값입니다.")
     private String name;
 }
