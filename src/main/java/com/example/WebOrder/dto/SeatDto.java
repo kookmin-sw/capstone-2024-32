960281fec8691ad3c92710466790664d27f9d488
moonwonki <moonwonki1999@gmail.com>
2024-03-27 20:14:07 +0900

    :sparkles: feat: Seat 변경에 따른 DTO 변경
    - 필드 뿐만 아니라 fromEntity 메서드에도 반영함

--- src/main/java/com/example/WebOrder/dto/SeatDto.java
+++ src/main/java/com/example/WebOrder/dto/SeatDto.java
@@ -17,11 +17,13 @@ public class SeatDto {
     private Long seatId;
     private String name;
     private String orders;
+    private Integer orderedTime;
 
     public static SeatDto fromEntity(Seat seat){
         SeatDto dto = new SeatDto();
         dto.setSeatId(seat.getId());
         dto.setName(seat.getName());
+        dto.setOrderedTime(dto.orderedTime);
 
         return dto;
     }
