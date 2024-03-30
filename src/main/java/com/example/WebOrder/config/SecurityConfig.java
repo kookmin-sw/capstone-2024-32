1d85c2a48d79b6bb69800c1bf24660b75e46780e
moonwonki <moonwonki1999@gmail.com>
2024-03-30 17:29:21 +0900

    :sparkles: feat: SecurityConfig 수정
    Refs: #23

--- src/main/java/com/example/WebOrder/config/SecurityConfig.java
+++ src/main/java/com/example/WebOrder/config/SecurityConfig.java
@@ -1,12 +1,11 @@
 package com.example.WebOrder.config;
 
+import com.example.WebOrder.controller.LoginController;
+import com.example.WebOrder.controller.LoginHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public LoginHandler loginHandler() {return new LoginHandler();}
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -32,7 +33,8 @@ public class SecurityConfig {
                 )
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
-                        .defaultSuccessUrl("/home")
+                        .successHandler(loginHandler())
+                        .failureHandler(loginHandler())
                         .permitAll()
                 )
                 .logout(logout -> logout
