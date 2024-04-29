2e2a73ff3ff831d7be75c2bd76146367696ae3ca
hightuv <hightuv@gmail.com>
2024-04-29 16:33:30 +0900

    :sparkles: feat: GuestController 분리
    - Order와 관련된 것은 OrderController로 옮김
    - 아직 다 옮기지는 않음, 추후 더 분리할 예정

--- src/main/java/com/example/WebOrder/controller/GuestController.java
+++ src/main/java/com/example/WebOrder/controller/GuestController.java
@@ -1,11 +1,16 @@
 package com.example.WebOrder.controller;
 
+import com.example.WebOrder.dto.OrderItemDto;
+import com.example.WebOrder.service.OrderService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Arrays;
 import java.util.Base64;
@@ -20,30 +25,18 @@ public class GuestController {
         return null;
     }
 
-    //비밀번호 인증을 성공했을 시 접근가능한 page
-    @GetMapping("/guest/{userId}/{seatId}")
-    public String getShopPageByGuest(@PathVariable Long userId, @PathVariable Long seatId){
-        return null;
-    }
-
-
 
     //주문하기를 눌렀을 때 접근가능한 page
-    @GetMapping("/guest/{userId}/{seatId}/order")
-    public String getOrderPageByGuest(@PathVariable Long userId, @PathVariable Long seatId){
-        return null;
-    }
+//    @GetMapping("/guest/{userId}/{seatId}/orderConfirm")
+//    public String getOrderPageByGuest(@PathVariable Long userId, @PathVariable Long seatId){
+//
+//        return null;
+//    }
 
 
     //메뉴 상세정보 보기
     @GetMapping("/guest/menu/{userId}/{menuId}")
     public String getMenuByGuest(@PathVariable Long userId, @PathVariable Long menuId){
-        return null;
-    }
-
-    //주문하기
-    @PostMapping("/guest/order/{encodedUserId}")
-    public String postOrderByGuest(@PathVariable String encodedUserId){
         return null;
     }
 
