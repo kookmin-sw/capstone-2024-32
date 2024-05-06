40da919a9b2394786ee3e5571f3f98e635158c6d
hightuv <hightuv@gmail.com>
2024-05-07 05:51:49 +0900

    :sparkles: feat: CategoryRepository 수정
    - Status 조건을 포함하여 조회하도록 수정함

--- src/main/java/com/example/WebOrder/repository/CategoryRepository.java
+++ src/main/java/com/example/WebOrder/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.example.WebOrder.repository;
 
 import com.example.WebOrder.entity.Category;
+import com.example.WebOrder.entity.CategoryStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,5 @@ import java.util.List;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    List<Category> findAllByAdminId(Long adminId);
+    List<Category> findAllByAdminIdAndStatus(Long adminId, CategoryStatus status);
 }
