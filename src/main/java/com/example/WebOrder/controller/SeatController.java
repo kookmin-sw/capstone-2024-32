3a96b7c37186c028cab5192ef16f2f3ba65eff89
moonwonki <moonwonki1999@gmail.com>
2024-05-06 19:02:11 +0900

    :sparkles: feat: SeatController 컨트롤러 URL 매핑
    - 테이블 현황 페이지 및 계산 페이지 매핑

--- src/main/java/com/example/WebOrder/controller/SeatController.java
+++ src/main/java/com/example/WebOrder/controller/SeatController.java
@@ -1,6 +1,7 @@
 package com.example.WebOrder.controller;
 
 import com.example.WebOrder.dto.SeatDto;
+import com.example.WebOrder.dto.SeatOrderDto;
 import com.example.WebOrder.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -8,20 +9,55 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
 public class SeatController {
     private final SeatService seatService;
+    private final LoginService loginService;
 
-    public SeatController(SeatService seatService) {
+    public SeatController(SeatService seatService, LoginService loginService) {
         this.seatService = seatService;
+        this.loginService = loginService;
     }
 
     //전체 테이블뷰 보기
     @GetMapping("/admin/seat/view")
-    public String getWholeSeatByOwner(){
-        return null;
+    public String getWholeSeatByOwner(Model model){
+        // 주문정보까지 포함된 좌석 정보 리스트
+        model.addAttribute("seatList", seatService.getWholeSeatOrdersOfCurrentUser());
+        model.addAttribute("seatNum", seatService.getTotalSeatNum());
+        return "seat/seatView";
+    }
+
+    // 테이블 비우기
+    @PostMapping("/admin/seat/view")
+    public String getSeatEmpty(Long seatId){
+        seatService.clearSeat(seatId);
+        return "redirect:/admin/seat/view";
+    }
+
+    //테이블 계산 페이지
+    @GetMapping("/admin/seat/bill/{seatId}")
+    public String getSeatBillPage(Model model, @PathVariable("seatId") Long seatId){
+        model.addAttribute("seat", seatService.getSeatOrdersOfSeat(loginService.getCurrentUserEntity().getId(), seatService.getSeatEntity(seatId)));
+        return "seat/seatBill";
+    }
+    
+    // api 활용 위해 ResponseBody 형태로 seat 계산서 리턴하기
+    @ResponseBody
+    @GetMapping("/admin/seat/bill/{seatId}/api")
+    public SeatOrderDto getSeatBilledApi(Model model, @PathVariable("seatId") Long seatId){
+
+        return seatService.getSeatOrdersOfSeat(loginService.getCurrentUserEntity().getId(), seatService.getSeatEntity(seatId));
+    }
+
+    // 테이블 계산하기
+    @PostMapping("/admin/seat/bill/{seatId}")
+    public String getSeatBilled(@PathVariable("seatId") Long seatId){
+        seatService.makeSeatBilled(seatId);
+        return "redirect:/admin/seat/view";
     }
 
     //테이블 관리 페이지
@@ -67,7 +103,7 @@ public class SeatController {
     }
     @PostMapping("/admin/seat/update/{seatId}")
     public String updateSeatByOwner(@PathVariable("seatId") Long seatId, String seatName){
-        seatService.updateSeat(seatId, seatName);
+        seatService.updateSeatName(seatId, seatName);
         return "redirect:/admin/seat/manage";
     }
 
