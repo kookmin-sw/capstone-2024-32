d776503b44a2fdded9a6dbe6c1340223ac5ad495
hightuv <hightuv@gmail.com>
2024-05-12 07:31:09 +0900

    :sparkles: feat: ProfileDto, UserEditFormDto 수정
    - User Entity 속성 추가에 따른 수정

--- src/main/java/com/example/WebOrder/dto/ProfileDto.java
+++ src/main/java/com/example/WebOrder/dto/ProfileDto.java
@@ -14,11 +14,19 @@ public class ProfileDto {
 
     private String name;
 
+    private String address;
+
+    private String description;
+
+    private String profileImageUrl;
+
     public static ProfileDto fromEntity(User user) {
         ProfileDto dto = new ProfileDto();
         dto.setUsername(user.getUsername());
         dto.setName(user.getName());
-
+        dto.setAddress(user.getAddress());
+        dto.setDescription(user.getDescription());
+        dto.setProfileImageUrl(user.getProfileImageUrl());
         return dto;
     }
 }
