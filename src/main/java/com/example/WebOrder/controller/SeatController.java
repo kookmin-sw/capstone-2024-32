349e6f09ea92bca70a70d09709b9f51b34a96636
hightuv <hightuv@gmail.com>
2024-05-13 07:44:10 +0900

    :sparkles: feat: SeatController 수정
    - 테이블 현황, 관리 모든 것을 seatView에서 하도록 했음

--- src/main/java/com/example/WebOrder/controller/SeatController.java
+++ src/main/java/com/example/WebOrder/controller/SeatController.java
@@ -37,6 +37,8 @@ public class SeatController {
         model.addAttribute("currentTime", formattedTime);
         model.addAttribute("seatList", seatService.getWholeSeatOrdersOfCurrentUser());
         model.addAttribute("seatNum", seatService.getTotalSeatNum());
+        model.addAttribute("mostOrderedSeat", seatService.getMostOrderedSeatOfCurrentUser()); //가장 많이 주문된 좌석
+        model.addAttribute("leastOrderedSeat", seatService.getLeastOrderedSeatOfCurrentUser()); //가장 적게 주문된 좌석
         return "seat/seatView";
     }
 
@@ -69,57 +71,31 @@ public class SeatController {
         return "redirect:/admin/seat/view";
     }
 
-    //테이블 관리 페이지
-    @GetMapping("/admin/seat/manage")
-    public String getSeatManagePageByOwner(Model model) {
-        model.addAttribute("mostOrderedSeat", seatService.getMostOrderedSeatOfCurrentUser()); //가장 많이 주문된 좌석
-        model.addAttribute("leastOrderedSeat", seatService.getLeastOrderedSeatOfCurrentUser()); //가장 적게 주문된 좌석
-        model.addAttribute("totalSeatNum", seatService.getTotalSeatNum()); //총 좌석 개수
-        model.addAttribute("seatList", seatService.getBasicSeatListOfCurrentUser()); //좌석 이름 리스트
-
-        return "seat/seatManage";
-    }
-
     //테이블 생성하기
-    @GetMapping("/admin/seat/create")
-    public String getCreateSeatForm(Model model){
-        model.addAttribute("seat", new SeatDto());
-        model.addAttribute("isCreate", true);
-        model.addAttribute("seatName", null);
-        return "seat/seatCreate";
-    }
     @PostMapping("/admin/seat/create")
     public String createSeatByOwner(String seatName){
         seatService.addSeat(seatName);
-        return "redirect:/admin/seat/manage";
+        return "redirect:/admin/seat/view";
     }
-
 
     //테이블 삭제하기
     @PostMapping("/admin/seat/delete/{seatId}")
     public String deleteSeatByOwner(@PathVariable("seatId") Long seatId){
         seatService.deleteSeat(seatId);
-        return "redirect:/admin/seat/manage";
+        return "redirect:/admin/seat/view";
     }
 
     //테이블 수정하기
-    @GetMapping("/admin/seat/update/{seatId}")
-    public String getUpdateSeatForm(@PathVariable("seatId") Long seatId, Model model){
-        model.addAttribute("seat", seatService.getSeatDto(seatId));
-        model.addAttribute("isCreate", false);
-        model.addAttribute("seatName", seatService.getSeatName(seatId));
-        return "seat/seatCreate";
-    }
     @PostMapping("/admin/seat/update/{seatId}")
     public String updateSeatByOwner(@PathVariable("seatId") Long seatId, String seatName){
         seatService.updateSeatName(seatId, seatName);
-        return "redirect:/admin/seat/manage";
+        return "redirect:/admin/seat/view";
     }
 
     //테이블 치우기
     @PostMapping("/admin/seat/clear/{seatId}")
     public String clearSeatByOwner(@PathVariable("seatId") Long seatId){
         seatService.clearSeat(seatId);
-        return "redirect:/admin/seat/manage";
+        return "redirect:/admin/seat/view";
     }
 }
