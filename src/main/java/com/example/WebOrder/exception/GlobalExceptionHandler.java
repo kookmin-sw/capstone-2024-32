33a17f6cec3822a0f2b13d8a15e88453b2490c97
moonwonki <moonwonki1999@gmail.com>
2024-05-14 02:52:30 +0900

    :sparkles: feat: GlobalExceptionHandler 추가
    - 예외처리 위한 핸들러 추가

---
+++ src/main/java/com/example/WebOrder/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.example.WebOrder.exception;
+
+import com.example.WebOrder.exception.status4xx.ForbiddenException;
+import com.example.WebOrder.exception.status4xx.NoEntityException;
+import com.example.WebOrder.exception.status4xx.NotAuthenticatedException;
+import com.example.WebOrder.exception.status4xx.TypicalException;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(TypicalException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleStatus400Exception(final TypicalException ex, Model model){
+        model.addAttribute("errorMsg", ex.getMessage());
+        return "error/error";
+    }
+
+    @ExceptionHandler(NotAuthenticatedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String handleStatus401Exception(final NotAuthenticatedException ex, Model model){
+        model.addAttribute("errorMsg", ex.getMessage());
+        return "error/error";
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String handleStatus403Exception(final ForbiddenException ex, Model model){
+        model.addAttribute("errorMsg", ex.getMessage());
+        return "error/error";
+    }
+
+    @ExceptionHandler(NoEntityException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleStatus404Exception(final NoEntityException ex, Model model){
+        model.addAttribute("errorMsg", ex.getMessage());
+        return "error/error";
+    }
+}
